a60df85baae94a5e6f06d60a6a754a0a692ce9bc
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2020-01-25 13:56:39 +0100

    docs(value): document `Value` and its variants

--- src/core/value.rs
+++ src/core/value.rs
@@ -1,8 +1,36 @@
+/// The only data type that can be actually pushed or pulled from a [`Stack`][Stack].
+///
+/// `Value` has four variants that should cover most use cases, namely: [`Boolean`][Boolean],
+/// [`Float`][Float], [`Integer`][Integer] or [`String`][String].
+///
+/// The point of `Value` is being used inside [`Script`s][Script] (wrapped in an [`Item`][Item]) or
+/// to be pushed into a [`Stack`][Stack].
+///
+/// As explained in [known limitations], _"the possible value types that can be pushed into the
+/// Stack is not generic nor customizable. Such feature will only be added if someone actually
+/// equests it"_.
+///
+/// `Value` does not implement any methods other than implementations of some traits from the
+/// [`core`][core] crate.
+///
+/// [Stack]: ../stack/struct.Stack.html
+/// [Boolean]: #variant.Boolean
+/// [Float]: #variant.Float
+/// [Integer]: #variant.Integer
+/// [String]: #variant.String
+/// [Script]: ../type.Script.html
+/// [Item]: ../item/enum.Item.html
+/// [known limitations]: ../../#known-limitations
+/// [core]: https://doc.rust-lang.org/nightly/core/
 #[derive(Clone, Debug)]
 pub enum Value {
+    /// A binary value: either `true` or `false`.
     Boolean(bool),
+    /// A signed floating point value.
     Float(f64),
+    /// A signed integer value.
     Integer(i128),
+    /// A string of characters.
     String(&'static str),
 }
 
